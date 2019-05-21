668a1df04faaaa0cfc69e54cd3a103096e1bcc5c
Stellan Garhammar <stellan.garhammar@kambi.com>
2019-05-21 12:03:28 +0200

    Trying to mix global and component variables

--- src/BunchOfOutcomeElements.js
+++ src/BunchOfOutcomeElements.js
@@ -7,21 +7,27 @@ class BunchOfOutcomeElements extends LitElement {
     super();
     this.buttons = Array(328);
     this.buttons.fill('Button', 0, 328);
+
+    //       <!-- ${this.buttons.map( i => html`<outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35"></outcome-element>`)} -->
   }
 
   static get styles() {
     return css`
       :host {
-        display: flex;
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr 1fr;
+        grid-column-gap: 1rem;
+        grid-row-gap: 1rem;
         justify-content: space-evenly;
         flex-wrap: wrap;
         padding: 2rem;
+        background-color: var(--myBackground);
       }
       
       div {
         width: 100%;
         height: 100%;
-        padding: var(--global-Margin, 2rem);
+        padding: 2rem;
         align-self: center;
       }
     `;
@@ -29,7 +35,35 @@ class BunchOfOutcomeElements extends LitElement {
 
   render() {
     return html`
-      ${this.buttons.map( i => html`<outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35"></outcome-element>`)}
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="ROW"></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="ROW" oddsIncrease></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="ROW" oddsDecrease></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="ROW" oddsIncrease persistedOdds></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="ROW" oddsDecrease persistedOdds></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="ROW" oddsIncrease selected></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="ROW" oddsDecrease selected></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="ROW" disabled></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="ROW" disabled oddsDecrease></outcome-element>
+
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="STACK"></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="STACK" oddsIncrease></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="STACK" oddsDecrease></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="STACK" oddsIncrease persistedOdds></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="STACK" oddsDecrease persistedOdds></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="STACK" oddsIncrease selected></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="STACK" oddsDecrease selected></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="STACK" disabled></outcome-element>
+      <outcome-element primaryLabel="Liverpool" secondaryLabel="(+1)" odds="2.35" layout="STACK" disabled oddsDecrease></outcome-element>
+
+      <outcome-element odds="2.35" layout="ROW-CENTERED"></outcome-element>
+      <outcome-element odds="2.35" layout="ROW-CENTERED" oddsIncrease></outcome-element>
+      <outcome-element odds="2.35" layout="ROW-CENTERED" oddsDecrease></outcome-element>
+      <outcome-element odds="2.35" layout="ROW-CENTERED" oddsIncrease persistedOdds></outcome-element>
+      <outcome-element odds="2.35" layout="ROW-CENTERED" oddsDecrease persistedOdds></outcome-element>
+      <outcome-element odds="2.35" layout="ROW-CENTERED" oddsIncrease selected></outcome-element>
+      <outcome-element odds="2.35" layout="ROW-CENTERED" oddsDecrease selected></outcome-element>
+      <outcome-element odds="2.35" layout="ROW-CENTERED" disabled></outcome-element>
+      <outcome-element odds="2.35" layout="ROW-CENTERED" disabled oddsDecrease></outcome-element>
     `;
   }
 }
--- src/outcome/OutcomeElement.js
+++ src/outcome/OutcomeElement.js
@@ -4,67 +4,98 @@ class OutcomeElement extends LitElement {
 
   static get styles() {
     return css`
+        
         :host {
             display: inline;
-            margin: 4px;
+            contain: content; // https://developers.google.com/web/fundamentals/web-components/shadowdom#stylefromoutside
         }
 
-        :host([persistedOdds]) .oddsIncrease,
-        :host([persistedOdds]) .oddsDecrease {
-            fill: white;
-        }
-   
         button {
             width: 100%;
             min-width: 64px;
             max-width: 124px;
             overflow: hidden;
             outline: none;
-
+            
             border: 0;
             border-radius: 2px;
-
+            
             padding: 8px;
-            background-color: #D291BC;
+            background-color: var(--outcome-button-backgroundColor);
             color: white;
             fontSize: 0.75rem
         }
 
         button:hover {
             cursor: pointer;
-            background-color: #FEC8D8;
+            background-color: var( --outcome-button-backgroundColor-hover);
         }
 
         :host([selected]) button {
             color: black;
-            background-color: #feffa3;
+            background-color: var(--outcome-button-selected-backgroundColor);
         }
 
         :host([selected]) button:hover {
-            background-color: #fafbbc;
+            background-color: var(--outcome-button-selected-backgroundColor-hover);
         }
 
         :host([disabled]) button {
-            cursor: default;
             color: gray;
             background-color: rgba(0, 0, 0, .24);
+        }
+        
+        :host([disabled]) button:hover {
+            cursor: default;
         }
 
         .wrapper {
             display: flex;
             width: 100%;
             flex-wrap: nowrap;
-            justify-content: space-between;
+        }
+
+        :host([layout="ROW"]) .wrapper,
+        :host([layout="ROW-CENTERED"]) .wrapper {
             flex-direction: row;
+        }
+
+        :host([layout="STACK"]) .wrapper {
+            flex-direction: column-reverse;
+        }
+
+        :host([layout="ROW"]) .wrapper,
+        :host([layout="STACK"]) .wrapper {
+            justify-content: space-between;
+        }
+
+        :host([layout="ROW-CENTERED"]) .wrapper {
+            justify-content: center;
         }
 
         .labelAndInfo {
             display: flex;
             overflow: hidden;
             white-space: nowrap;
-            padding-right: 4px;
             min-height: 1rem; // this should be relative to font-size?
+        }
+
+        :host([layout="ROW"]) .labelAndInfo,
+        :host([layout="ROW-CENTERED"]) .labelAndInfo {
             justify-content: flex-start;
+        }
+
+        :host([layout="STACK"]) .labelAndInfo {
+            justify-content: center;
+        }
+
+        :host([layout="ROW"]) .wrapper,
+        :host([layout="STACK"]) .wrapper {
+           padding-right: 4px;
+        }
+
+        :host([layout="ROW-CENTERED"]) .wrapper {
+            padding-right: none;
         }
 
         .label {
@@ -80,7 +111,15 @@ class OutcomeElement extends LitElement {
         
         .oddsWrapper {
             display: flex;
+        }
+
+        :host([layout="ROW"]) .oddsWrapper,
+        :host([layout="ROW-CENTERED"]) .oddsWrapper {
             justify-content: flex-start;
+        }
+
+        :host([layout="STACK"]) .oddsWrapper {
+            justify-content: center;
         }
 
         .odds {
@@ -91,12 +130,28 @@ class OutcomeElement extends LitElement {
             visibility: visible;
         }
 
+        :host([disabled]) .odds {
+            visibility: hidden;
+        }
+
+        :host([oddsIncrease][layout="STACK"]) .odds,
+        :host([oddsDecrease][layout="STACK"]) .odds,
+        :host([oddsIncrease][layout="ROW-CENTERED"]) .odds,
+        :host([oddsDecrease][layout="ROW-CENTERED"]) .odds {
+           margin-right: 8px;
+        }
+
         .oddsIndicator {
             width: 6px;
             display: flex;
             flex-direction: column;
             justify-content: center;
             margin-right: 2px;
+        }
+
+        :host([disabled][oddsDecrease]) .oddsIndicator,
+        :host([disabled][oddsIncrease]) .oddsIndicator {
+            display: none;
         }
 
         .oddsIncrease {
@@ -106,7 +161,25 @@ class OutcomeElement extends LitElement {
         .oddsDecrease {
             fill: red;
         }
+
+        :host([persistedOdds]) .oddsIncrease,
+        :host([persistedOdds]) .oddsDecrease {
+            fill: white;
+        }
     `;
+  }
+
+  constructor() {
+      super();
+      this.layout = "ROW";
+      this.primaryLabel = "";
+      this.secondaryLabel = "";
+      this.odds = "";
+      this.selected = false;
+      this.disabled = false;
+      this.oddsIncrease = false;
+      this.oddsDecrease = false;
+      this.persistedOdds = false;
   }
 
   static get properties() {
@@ -116,9 +189,10 @@ class OutcomeElement extends LitElement {
         odds: { type: String },
         selected: { type: Boolean },
         disabled: { type: Boolean },
-        // layout: 'ROW',
+        layout: { type: String },
         oddsIncrease: { type: Boolean },
-        oddsDecrease: { type: Boolean } 
+        oddsDecrease: { type: Boolean },
+        persistedOdds: { type: Boolean }
     };
   }
 
@@ -134,21 +208,23 @@ class OutcomeElement extends LitElement {
                     <div class="label">${this.primaryLabel}</div>
                     <div class="info">${this.secondaryLabel}</div>
                 </div>
-                <div class="oddsIndicator">
-                    ${this.oddsIncrease ?
-                        html`<svg x="0" y="0" viewBox="0 0 100 80" preserveAspectRatio="none">
-                            <polygon points="50 0,100 80, 0 80" class="oddsIncrease" />
-                        </svg>` :
-                        html``
-                    }
-                    ${this.oddsDecrease ?
-                        html`<svg x="0" y="0" viewBox="0 0 100 80" preserveAspectRatio="none">
-                            <polygon points="0 0, 100 0, 50 80" class="oddsDecrease" />
-                        </svg>` :
-                        html``
-                    }
+                <div class="oddsWrapper">
+                    <div class="oddsIndicator">
+                        ${this.oddsIncrease ?
+                            html`<svg x="0" y="0" viewBox="0 0 100 80" preserveAspectRatio="none">
+                                <polygon points="50 0,100 80, 0 80" class="oddsIncrease" />
+                            </svg>` :
+                            ''
+                        }
+                        ${this.oddsDecrease ?
+                            html`<svg x="0" y="0" viewBox="0 0 100 80" preserveAspectRatio="none">
+                                <polygon points="0 0, 100 0, 50 80" class="oddsDecrease" />
+                            </svg>` :
+                            ''
+                        }
+                    </div>
+                    <div class="odds">${this.odds}</div>
                 </div>
-                <div class="odds">${this.odds}</div>
             </div>
             </div>
         </button>
